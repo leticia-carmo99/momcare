40a07beccf69cf25839fe32919c9fd21cf52ba5a
natizsantos <nspaschoa@gmail.com>
2025-10-08 20:58:21 +0000

    atualizações

--- MomCare/App.js
+++ MomCare/App.js
@@ -35,7 +35,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="AddArticle" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="WelcomeMother" component={WelcomeMotherScreen} />
         <Stack.Screen name="LoginMother" component={LoginMotherScreen} />
         <Stack.Screen name="RegisterMother" component={RegisterMotherScreen} />
